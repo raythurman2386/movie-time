5aabc5cd6982f1cfd00cbccb532995f3bb25db6c
Raymond Thurman <raymondthurman5@gmail.com>
2019-05-27 13:29:24 -0400

    proptypes set up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const movies = [
+	{
+		id    : 1,
+		title : 'Star Wars',
+		desc  : 'Movie about a man and his father',
+	},
+	{
+		id    : 2,
+		title : 'Spider Man',
+	},
+	{
+		id    : 3,
+		title : 'Iron Man',
+	},
+];
+
+class App extends Component {
+	render() {
+		return (
+			<div className='App'>
+				<header className='App-header'>
+					<img src={logo} className='App-logo' alt='logo' />
+				</header>
+				{movies.map((movie) => <Movie key={movie.id} movie={movie} desc={movie.desc} />)}
+			</div>
+		);
+	}
 }
 
 export default App;
