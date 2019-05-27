a9207c82f44e941e54d8d50bc16923ca286ba31d
Raymond Thurman <raymondthurman5@gmail.com>
2019-05-27 14:17:54 -0400

    refactored movie to a functional component

--- src/Movie.js
+++ src/Movie.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class Movie extends Component {
-	static propTypes = {
-		movie : PropTypes.shape({
-			title : PropTypes.string.isRequired,
-		}),
-	};
+const Movie = ({ movie }) => (
+	<div>
+		<h3>{movie.title}</h3>
+		<p>{movie.overview}</p>
+	</div>
+);
 
-	render() {
-		return (
-			<div>
-				<h3>{this.props.movie.title}</h3>
-				<p>{this.props.desc}</p>
-			</div>
-		);
-	}
-}
+export default Movie;
+
+Movie.propTypes = {
+	movie : PropTypes.shape({
+		title : PropTypes.string.isRequired,
+	}).isRequired,
+};
