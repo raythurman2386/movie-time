e470682053d89fcdc364391631a5098183fe6429
raythurman2386 <raymondthurman5@gmail.com>
2019-06-28 22:44:10 -0400

    basic react router usage

--- src/Movie.jsx
+++ src/Movie.jsx
@@ -3,16 +3,16 @@ import PropTypes from 'prop-types';
 
 const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
 
-const Movie = ({ movie }) => ( 
+const Movie = ({ movie }) => (
 	<div>
-		<img src = {`${POSTER_PATH}${movie.poster_path}`}	alt = '' / >
+		<img src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
 	</div>
 );
 
 export default Movie;
 
 Movie.propTypes = {
-	movie: PropTypes.shape({
-		title: PropTypes.string.isRequired,
+	movie : PropTypes.shape({
+		title : PropTypes.string.isRequired,
 	}).isRequired,
 };
