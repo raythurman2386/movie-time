176999fbf07768d22fdd8ec82fa12398757a5908
raythurman2386 <raymondthurman5@gmail.com>
2019-06-30 21:17:03 -0400

    updating my hook

---
+++ src/components/PracticeHook.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { Poster } from './Movie';
+import Overdrive from 'react-overdrive';
+
+const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
+const BACKDROP_PATH = 'http://image.tmdb.org/t/p/w1280';
+
+const MovieDetail = () => (
+  const [movie, setMovie] = useState({});
+
+	useEffect(()=>{
+    try {
+      const result = await fetch(
+        `https://api.themoviedb.org/3/movie/${movie.match.params
+          .id}?api_key=3e11806009cadfb91187ad7b65b9dc21&language=en-US`,
+      );
+      movie = await result.json();
+
+      setMovie({
+        movie
+      });
+
+    } catch (e) {
+      console.log(e);
+    }
+  })
+
+		return (
+			<MovieWrapper backdrop={`${BACKDROP_PATH}${movie.backdrop_path}`}>
+				<MovieInfo>
+					<Link to='/'>
+						<Overdrive id={movie.id}>
+							<Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+						</Overdrive>
+					</Link>
+					<div>
+						<h1>{movie.title}</h1>
+						<h3>{movie.release_date}</h3>
+						<p>{movie.overview}</p>
+					</div>
+				</MovieInfo>
+			</MovieWrapper>
+		);
+)
+
+export default MovieDetail;
+
+const MovieWrapper = styled.div`
+	position: relative;
+	padding-top: 50vh;
+	background: url(${(props) => props.backdrop}) no-repeat center;
+	background-size: cover;
+`;
+
+const MovieInfo = styled.div`
+	background: crimson;
+	text-shadow: #999 0px 0px 15px;
+	text-align: left;
+	padding: 2rem 10%;
+	display: flex;
+	> div {
+		margin-left: 20px;
+	}
+	img {
+		position: relative;
+		top: -5rem;
+	}
+	@media (max-width: 500px) {
+		flex-direction: column;
+		padding: 1rem 5%;
+	}
+`;
