b593fe8c62fc509441919ba151ca2d50f2526664
raythurman2386 <raymondthurman5@gmail.com>
2019-07-20 22:21:59 -0400

    fixed so I can branch

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,64 +1,64 @@
 import React, { PureComponent, useState, useEffect } from 'react';
 import styled from 'styled-components';
-import axios from 'axios';
+// import axios from 'axios';
 
 import Movie from './Movie';
 
-// class MoviesList extends PureComponent {
-// 	state = {
-// 		movies : [],
-// 	};
+class MoviesList extends PureComponent {
+	state = {
+		movies : [],
+	};
 
-// 	async componentDidMount() {
-// 		try {
-// 			const result = await fetch(
-// 				'https://api.themoviedb.org/3/discover/movie?api_key=3e11806009cadfb91187ad7b65b9dc21&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1',
-// 			);
-// 			const movies = await result.json();
-
-// 			this.setState({
-// 				movies : movies.results,
-// 			});
-// 		} catch (e) {
-// 			console.log(e);
-// 		}
-// 	}
-
-// 	render() {
-// 		console.log(this.state.movies);
-// 		return (
-// 			<MovieGrid>
-// 				{this.state.movies.map((movie) => (
-// 					<Movie key={movie.id} movie={movie} />
-// 				))}
-// 			</MovieGrid>
-// 		);
-// 	}
-// }
-
-const MoviesList = () => {
-	const [ movies, setMovies ] = useState({});
-
-	useEffect(() => {
-		axios
-			.get(
+	async componentDidMount() {
+		try {
+			const result = await fetch(
 				'https://api.themoviedb.org/3/discover/movie?api_key=3e11806009cadfb91187ad7b65b9dc21&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1',
-			)
-			.then((res) => {
-				const movies = res.json();
-				setMovies(movies.results);
-			})
-			.catch((err) => console.log(err));
-	}, []);
+			);
+			const movies = await result.json();
 
-	console.log(movies);
+			this.setState({
+				movies : movies.results,
+			});
+		} catch (e) {
+			console.log(e);
+		}
+	}
 
-	return (
-		<MovieGrid>
-			{movies.map((movie) => <Movie key={movie.id} movie={movie} />)}
-		</MovieGrid>
-	);
-};
+	render() {
+		console.log(this.state.movies);
+		return (
+			<MovieGrid>
+				{this.state.movies.map((movie) => (
+					<Movie key={movie.id} movie={movie} />
+				))}
+			</MovieGrid>
+		);
+	}
+}
+
+// const MoviesList = () => {
+// 	const [ movies, setMovies ] = useState({});
+
+// 	useEffect(() => {
+// 		axios
+// 			.get(
+// 				'https://api.themoviedb.org/3/discover/movie?api_key=3e11806009cadfb91187ad7b65b9dc21&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1',
+// 			)
+// 			.then((result) => {
+// 				const movies = result.json();
+// 				setMovies(movies.results);
+// 			})
+// 			.catch((err) => console.log(err));
+// 	}, []);
+
+// 	console.log(movies);
+
+// 	return (
+// 		<MovieGrid>
+// 			{movies.map((movie) => <Movie key={movie.id} movie={movie} />)}
+// 		</MovieGrid>
+// 	);
+// };
 
 export default MoviesList;
 
