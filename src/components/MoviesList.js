e2886760f2a38e9ecb4841f387ecc5816b438c42
raythurman2386 <raymondthurman5@gmail.com>
2019-07-20 22:52:21 -0400

    removed class component from the hook

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,64 +1,35 @@
-import React, { PureComponent, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
 
 import Movie from './Movie';
 
-// class MoviesList extends PureComponent {
-// 	state = {
-// 		movies : [],
-// 	};
-
-// 	async componentDidMount() {
-// 		try {
-// 			const result = await fetch(
-// 				'https://api.themoviedb.org/3/discover/movie?api_key=3e11806009cadfb91187ad7b65b9dc21&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1',
-// 			);
-// 			const movies = await result.json();
-
-// 			this.setState({
-// 				movies : movies.results,
-// 			});
-// 		} catch (e) {
-// 			console.log(e);
-// 		}
-// 	}
-
-// 	render() {
-// 		console.log(this.state.movies);
-// 		return (
-// 			<MovieGrid>
-// 				{this.state.movies.map((movie) => (
-// 					<Movie key={movie.id} movie={movie} />
-// 				))}
-// 			</MovieGrid>
-// 		);
-// 	}
-// }
-
-// Emplementing Hook
-
 const MoviesList = () => {
 	// movies hook for state
-	const [ movies, setMovies ] = useState({});
+	const [ movies, setMovies ] = useState({
+		movies : [],
+	});
 
 	// hook for api
-	useEffect(() => {
-		axios
-			// get data from the API
-			.get(
-				'https://api.themoviedb.org/3/discover/movie?api_key=3e11806009cadfb91187ad7b65b9dc21&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1',
-			)
-			// set the results to state
-			.then((result) => {
-				const movies = result.json();
-				setMovies(movies.results);
-			})
-			// catch errors
-			.catch((err) => console.log(err));
-	}, []);
+	useEffect(
+		() => {
+			axios
+				// get data from the API
+				.get(
+					'https://api.themoviedb.org/3/discover/movie?api_key=3e11806009cadfb91187ad7b65b9dc21&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1',
+				)
+				// set the results to state
+				.then((result) => {
+					// console.log(result, 'result');
+					setMovies({ movies: result.data.json() });
+				})
+				// catch errors
+				.catch((err) => console.log(err));
+		},
+		[ movies ],
+	);
 
-	console.log(movies);
+	console.log(movies.movies, 'movie');
 
 	return (
 		<MovieGrid>
