47aa561453c09fe6bcc27a0a9f634798a9eefae4
raythurman2386 <raymondthurman5@gmail.com>
2019-07-20 22:25:54 -0400

    Emplemented hook and added comments

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,64 +1,72 @@
 import React, { PureComponent, useState, useEffect } from 'react';
 import styled from 'styled-components';
-// import axios from 'axios';
+import axios from 'axios';
 
 import Movie from './Movie';
 
-class MoviesList extends PureComponent {
-	state = {
-		movies : [],
-	};
+// class MoviesList extends PureComponent {
+// 	state = {
+// 		movies : [],
+// 	};
 
-	async componentDidMount() {
-		try {
-			const result = await fetch(
-				'https://api.themoviedb.org/3/discover/movie?api_key=3e11806009cadfb91187ad7b65b9dc21&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1',
-			);
-			const movies = await result.json();
-
-			this.setState({
-				movies : movies.results,
-			});
-		} catch (e) {
-			console.log(e);
-		}
-	}
-
-	render() {
-		console.log(this.state.movies);
-		return (
-			<MovieGrid>
-				{this.state.movies.map((movie) => (
-					<Movie key={movie.id} movie={movie} />
-				))}
-			</MovieGrid>
-		);
-	}
-}
-
-// const MoviesList = () => {
-// 	const [ movies, setMovies ] = useState({});
-
-// 	useEffect(() => {
-// 		axios
-// 			.get(
+// 	async componentDidMount() {
+// 		try {
+// 			const result = await fetch(
 // 				'https://api.themoviedb.org/3/discover/movie?api_key=3e11806009cadfb91187ad7b65b9dc21&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1',
-// 			)
-// 			.then((result) => {
-// 				const movies = result.json();
-// 				setMovies(movies.results);
-// 			})
-// 			.catch((err) => console.log(err));
-// 	}, []);
+// 			);
+// 			const movies = await result.json();
 
-// 	console.log(movies);
+// 			this.setState({
+// 				movies : movies.results,
+// 			});
+// 		} catch (e) {
+// 			console.log(e);
+// 		}
+// 	}
 
-// 	return (
-// 		<MovieGrid>
-// 			{movies.map((movie) => <Movie key={movie.id} movie={movie} />)}
-// 		</MovieGrid>
-// 	);
-// };
+// 	render() {
+// 		console.log(this.state.movies);
+// 		return (
+// 			<MovieGrid>
+// 				{this.state.movies.map((movie) => (
+// 					<Movie key={movie.id} movie={movie} />
+// 				))}
+// 			</MovieGrid>
+// 		);
+// 	}
+// }
+
+// Emplementing Hook
+
+const MoviesList = () => {
+	// movies hook for state
+	const [ movies, setMovies ] = useState({});
+
+	// hook for api
+	useEffect(() => {
+		axios
+			// get data from the API
+			.get(
+				'https://api.themoviedb.org/3/discover/movie?api_key=3e11806009cadfb91187ad7b65b9dc21&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1',
+			)
+			// set the results to state
+			.then((result) => {
+				const movies = result.json();
+				setMovies(movies.results);
+			})
+			// catch errors
+			.catch((err) => console.log(err));
+	}, []);
+
+	console.log(movies);
+
+	return (
+		<MovieGrid>
+			{/* { Loop over data and display } */}
+			{movies.map((movie) => <Movie key={movie.id} movie={movie} />)}
+		</MovieGrid>
+	);
+};
 
 export default MoviesList;
 
@@ -67,6 +75,6 @@ const MovieGrid = styled.div`
 	margin: 0 auto;
 	display: grid;
 	padding: 1rem;
-	grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+	grid-template-columns: repeat(auto-fill);
 	grid-row-gap: 2rem;
 `;
