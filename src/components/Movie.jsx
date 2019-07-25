02ec181339c1fb565cd157facbfcd8303704ca44
Raymond Thurman <raymondthurman5@gmail.com>
2019-07-25 08:16:49 -0400

    minor styling changes
    Still in the process of working on the search bar

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,31 +1,34 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import Overdrive from 'react-overdrive';
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import Overdrive from "react-overdrive";
 
-const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
+const POSTER_PATH = "http://image.tmdb.org/t/p/w154";
 
 const Movie = ({ movie }) => (
-	<Link to={`/${movie.id}`}>
-		<Overdrive id={movie.id}>
-			<Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
-		</Overdrive>
-	</Link>
+  <Link to={`/${movie.id}`}>
+    <Overdrive id={movie.id}>
+      <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+    </Overdrive>
+  </Link>
 );
 
 export default Movie;
 
 Movie.propTypes = {
-	movie : PropTypes.shape({
-		title : PropTypes.string.isRequired,
-	}).isRequired,
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired
+  }).isRequired
 };
 
 export const Poster = styled.img`
-	box-shadow: 0 0 35px black;
-	:hover {
-		box-shadow: 0 0 35px white;
-		transform: scale(1.2);
-	}
+  box-shadow: 0 0 35px black;
+  :hover {
+    box-shadow: 0 0 35px white;
+    transform: scale(1.2);
+  }
+  /* @media (max-width: 500px) {
+		width: 240px;
+	} */
 `;
