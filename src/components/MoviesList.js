3ea73db486d599d4fdab543d6f804bae74f59246
raythurman2386 <raymondthurman5@gmail.com>
2019-07-31 20:23:40 -0400

    changed movies to now playing

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -14,8 +14,11 @@ const MoviesList = () => {
 	useEffect(() => {
 		axios
 			// get data from the API
+			// .get(
+			// 	'https://api.themoviedb.org/3/discover/movie?api_key=3e11806009cadfb91187ad7b65b9dc21&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1',
+			// )
 			.get(
-				'https://api.themoviedb.org/3/discover/movie?api_key=3e11806009cadfb91187ad7b65b9dc21&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1',
+				`https://api.themoviedb.org/3/movie/now_playing?api_key=3e11806009cadfb91187ad7b65b9dc21&language=en-US&page=1`,
 			)
 			// set the results to state
 			.then((response) => {
