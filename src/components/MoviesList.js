9d530b0914ada712afce24ebf159a266d905e963
Thurman <XUPD48RET@Whaynenet.com>
2019-08-27 07:47:12 -0400

    fixed the broken movies list component
    Movies list had broken when I merged my branch to master and I wasn't paying attention.
    
    Will need refactored back to hooks

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,14 +1,31 @@
-import React, { useState, useEffect } from 'react';
+/* eslint react/no-did-mount-set-state: 0 */
+import React, { PureComponent } from 'react';
 import styled from 'styled-components';
-import axios from 'axios';
-
 import Movie from './Movie';
 
-const MoviesList = () => {
-	// movies hook for state
-	const [ movies, setMovies ] = useState({
-		movies : [],
+class MoviesList extends PureComponent {
+	state = {
+		movies: [],
+	};
 
+	async componentDidMount() {
+		try {
+			const res = await fetch(
+				'https://api.themoviedb.org/3/discover/movie?api_key=65e043c24785898be00b4abc12fcdaae&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1',
+			);
+			const movies = await res.json();
+			this.setState({
+				movies: movies.results,
+			});
+		} catch (e) {
+			console.log(e);
+		}
+	}
+
+	render() {
+		return <MovieGrid>{this.state.movies.map((movie) => <Movie key={movie.id} movie={movie} />)}</MovieGrid>;
+	}
+}
 
 export default MoviesList;
 
