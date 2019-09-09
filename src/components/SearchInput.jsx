ba2b61a80e34e9f5bbceb50423440f1e7a04f1b3
raythurman2386 <raymondthurman5@gmail.com>
2019-09-08 20:33:29 -0400

    search functionality starting to work

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import MovieSearch from './MovieSearch'
+import axios from 'axios'
 
 const SearchInput = props => {
+  // movies hook for state
+  const [movie, setMovie] = useState([])
+  // console.log(props, 'props');
+  // hook for api
+  useEffect(() => {
+    axios
+      // get data from the API
+      .get('https://api.themoviedb.org/3/search/movie?', {
+        params: {
+          api_key: '3e11806009cadfb91187ad7b65b9dc21',
+          language: 'en_US',
+          query: 'Jack Reacher',
+        },
+      })
+      // set the results to state
+      .then(res => {
+        console.log(res.data.results, 'response')
+        setMovie({ movie: res.data })
+      })
+      // catch errors
+      .catch(err => console.log(err))
+  }, [])
+
   return (
     <form>
       <Input
@@ -10,7 +34,7 @@ const SearchInput = props => {
         className='search-input'
         name='movie-search'
         placeholder='...Search'
-        value={props.title}
+        value={movie.title}
         onSubmit={MovieSearch}
       />
     </form>
